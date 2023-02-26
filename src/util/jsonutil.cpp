3f838c7813e9a1a48e101d8f61eb5df89f7bf262
dfyockey <git@diffengine.net>
2023-02-26 17:46:49 -0500

    Add explanatory comments

--- src/util/jsonutil.cpp
+++ src/util/jsonutil.cpp
@@ -7,9 +7,18 @@
 
 #include "jsonutil.hpp"
 
+/*
+ * In the following two functions, "obj" contains the JSON object from which to
+ * get a value, and "props" is an array of properties defining the hierarchical
+ * path leading to the desired JSON value, where the last item in the array is
+ * the JSON value desired.
+ *
+ */
+
 bjs::value jsonutil::getvalue(bjs::object& obj, std::vector<string>& props) {
 	bjs::value val = obj;
 
+	// Drill down through JSON hierarchy until desired value is found
 	for ( auto prop : props ) {
 		val = val.at(prop);
 	}
