932b87d6c20472958084fbde4c1820d0c4aebf6d
dfyockey <git@diffengine.net>
2023-03-28 17:13:35 -0400

    Add licensing

--- src/Curler.cpp
+++ src/Curler.cpp
@@ -1,8 +1,21 @@
 /*
  * Curler.cpp
+ * - Class Curler method implementations
  *
- *  Created on: Aug 3, 2022
- *      Author: David Yockey
+ * Copyright 2022 David Yockey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #include "Curler.h"
